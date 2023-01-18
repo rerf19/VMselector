5e2b2ed427382e9157769ba94dad6852a07e4634
Rodrigo Ferreira <r.reisferreira@lancaster.ac.uk>
2023-01-18 18:05:00 +0000

    set up the server and the basics

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
-const http = require('http')
-const port = 8080
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyparser = require('body-parser');
+const path = require('path');
 
-//server object
-const server = http.createServer(function (req, res) {
-})
+const app = express();
 
-// listen the port
-server.listen(port, function (error) {
-    console.log("Server Running...");
-})
+
+dotenv.config({path:'config.env'});
+const PORT = process.env.PORT||8080
+
+//log request
+app.use(morgan('tiny'))
+
+//parse request to body-parser
+app.use(bodyparser.urlencoded({extended:true}));
+
+//set view engine
+app.set("view engine","ejs");
+//app.set("views",path.resolve(__dirname,"views"))
+
+//load assets
+app.use("/css",express.static(path.resolve(__dirname,"assets/css")));
+app.use("/img",express.static(path.resolve(__dirname,"assets/img")));
+app.use("/js",express.static(path.resolve(__dirname,"assets/js")));
+
+//load routers
+app.use("/",require("./server/routes/router"));
+
+app.listen(PORT, () => {console.log(`Server is running on http://localhost:${PORT}`)});
