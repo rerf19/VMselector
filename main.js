44147a78cf836404b629cad8dac1e8e0698daba5
Rodrigo Ferreira <r.reisferreira@lancaster.ac.uk>
2023-01-18 13:40:16 +0000

    npm and dependencies

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const http = require('http')
 const port = 8080
 
-// Create a server object:
+//server object
 const server = http.createServer(function (req, res) {
 })
 
-// Set up our server so it will listen on the port
+// listen the port
 server.listen(port, function (error) {
     console.log("Server Running...");
 })
