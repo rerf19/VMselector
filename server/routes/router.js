dcd0eec78eaef6589cee0357e59dedbc66768462
Rodrigo Ferreira <r.reisferreira@lancaster.ac.uk>
2023-01-25 14:37:08 +0000

    azure page creation

--- server/routes/router.js
+++ server/routes/router.js
@@ -4,5 +4,6 @@ const route = express.Router();
 const services = require('../services/render')
 
 route.get('/', services.homeRoute);
+route.get('/provider/azure', services.azure);
 
 module.exports = route;
--- server/services/render.js
+++ server/services/render.js
@@ -1,3 +1,7 @@
 exports.homeRoute = (req,res) => {
     res.render('index');
 }
+
+exports.azure = (req,res) => {
+    res.render('azure');
+}
