274bb29db30d3ef53a4a20eb45a0ac57dd7b7649
Rodrigo Ferreira <r.reisferreira@lancaster.ac.uk>
2023-01-26 16:15:25 +0000

    azure page structure done

--- server/routes/router.js
+++ server/routes/router.js
@@ -4,6 +4,6 @@ const route = express.Router();
 const services = require('../services/render')
 
 route.get('/', services.homeRoute);
-route.get('/provider/azure', services.azure);
+route.get('/azure', services.azure);
 
 module.exports = route;
